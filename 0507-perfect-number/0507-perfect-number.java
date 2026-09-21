308a4274d3d561bd9ec5cea1041d5d508ff42c8c
Mohd Sarim <mohd.sarim202@gmail.com>
2026-09-21 13:52:23 +0530

    Time: 2 ms (85.29%), Space: 42.1 MB (44.46%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,16 +1,25 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num/2;i++)
-        {
-            if(num%i==0)
-            sum+=i;
-            if(sum>num)
+        // Numbers <= 1 are not perfect numbers
+        if (num <= 1) {
             return false;
         }
-        if(sum==num)
-        return true;
-        return false;
-        
+
+        // Start total with 1 since 1 is always a proper divisor
+        int total = 1;
+
+        // Loop up to sqrt(num)
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                total += i;
+                
+                // Add paired divisor if it's not a duplicate (e.g., perfect square root)
+                if (i != num / i) {
+                    total += num / i;
+                }
+            }
+        }
+
+        return total == num;
     }
 }
